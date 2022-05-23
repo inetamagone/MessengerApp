841ebb3b4c0e83f510e820e4323df1cae6cd22b5
inetamagone <99735096+inetamagone@users.noreply.github.com>
2022-05-23 08:46:30 +0300

    update

--- app/src/main/java/com/example/messengerapp/adapters/UserAdapter.kt
+++ app/src/main/java/com/example/messengerapp/adapters/UserAdapter.kt
@@ -1,31 +1,67 @@
 package com.example.messengerapp.adapters
 
 import android.content.Context
-import android.service.autofill.UserData
+
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.messengerapp.R
 import com.example.messengerapp.databinding.SearchItemBinding
+import com.example.messengerapp.model.UserData
+import com.squareup.picasso.Picasso
 
-class UserAdapter(private val context: Context, private val userList: List<UserData>, private val isOnline: Boolean): RecyclerView.Adapter<UserAdapter.UserViewHolder?>() {
+class UserAdapter(
+    private val context: Context, private val userList: List<UserData>,
+    private var isOnline: Boolean
+) : RecyclerView.Adapter<UserAdapter.UserViewHolder?>() {
 
-    class UserViewHolder(private val binding: SearchItemBinding): RecyclerView.ViewHolder(binding.root) {
+    var lastMessage: String = ""
+
+    inner class UserViewHolder(private val binding: SearchItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(userData: UserData) {
-            binding.apply {
-
-            }
+            binding.searchUsername.text = userData.getUsername()
+            Picasso
+                .get()
+                .load(userData.getProfile())
+                .placeholder(R.drawable.profile)
+                .into(binding.searchImage)
+            setOnlineStatus(isOnline, userData, binding)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        return UserViewHolder(SearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return UserViewHolder(
+            SearchItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: UserAdapter.UserViewHolder, position: Int) =
         holder.bind(userList[position])
-    }
 
-    override fun getItemCount(): Int {
-        return userList.size
+    override fun getItemCount(): Int =
+        userList.size
+
+    fun setOnlineStatus(isOnline: Boolean, userData: UserData, binding: SearchItemBinding) {
+        when {
+            isOnline -> {
+                if (userData.getStatus() == "online") {
+                    binding.searchOnline.visibility = View.VISIBLE
+                    binding.searchOffline.visibility = View.GONE
+                } else {
+                    binding.searchOnline.visibility = View.GONE
+                    binding.searchOffline.visibility = View.VISIBLE
+                }
+            }
+            else -> {
+                binding.searchOnline.visibility = View.GONE
+                binding.searchOffline.visibility = View.GONE
+            }
+        }
     }
 }
--- app/src/main/java/com/example/messengerapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/messengerapp/fragments/ChatFragment.kt
@@ -5,22 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.messengerapp.R
-
+import com.example.messengerapp.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentChatBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+    ): View {
+        _binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
